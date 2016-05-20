afc07975a7a05cc346689448a266a01d1b098e25
bkoester <ben.koester@gmail.com>
2016-05-20 10:41:40 -0400

    base layout,content of pages completed

---
+++ js/navBarScript.js
@@ -0,0 +1,8 @@
+//this fills in the navigation bar on all pages and makes it uniform
+document.getElementById("nav").innerHTML =
+     '<li><a href="index.html">Home</a></li>'+
+     '<li><a href="grade_prediction.html">Grade Prediction</a></li>'+
+     '<li><a href="persistence.html">Persistence</a></li>'+
+     '<li><a href="populations.html">Student Populations</a></li>'+
+     '<li><a href="progress.html">Progress to Major</a></li>'+
+     '<li><a href="student_network.html">Student Networks</a></li>';
